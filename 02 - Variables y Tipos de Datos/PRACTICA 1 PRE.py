eea70e2adf61b05b69d90f38ec87b5e67d442525
JORGE TORRADO <jorgeencucuta@hotmail.com>
2022-11-22 15:11:16 -0500

    gif

---
+++ 02 - Variables y Tipos de Datos/PRACTICA 1 PRE.py
@@ -0,0 +1,58 @@
+#1.Crear una variable que contenga un elemento del conjunto de números enteros y luego imprimir por pantalla
+from pickle import TRUE
+
+
+a= 19
+print(a)
+#2.Imprimir el tipo de dato de la constante 8.5
+print(type(8.5))
+#3.Imprimir el tipo de dato de la variable creada en el punto 1
+print(type(a))
+#4.	Crear una variable que contenga tu nombre
+my_name= "Jorge Torrado"
+#5.	Crear una variable que contenga un número complejo
+b= 6j
+#5.Crear una variable que contenga un número complejo
+print(type(b))
+#7.	Crear una variable que contenga el valor del número Pi redondeado a 4 decimales
+pi=3,1416
+print("pi=",pi)
+#8.	Crear una variable que contenga el valor 'True' y otra que contenga el valor True. ¿Se trata de lo mismo?
+l=True
+k=True
+print(l or k)
+#9.Imprimir el tipo de dato correspondientes a las variables creadas en el punto 9	Imprimir el tipo de dato correspondientes a las variables creadas en el punto 9
+print(type(l))
+print(type(k))
+#10.Asignar a una variable, la suma de un número entero y otro decimal
+s=(10+2.8)
+print(s)
+#11.Realizar una operación de suma de números complejos
+x= 5j + 7j
+print(x)
+#12.Realizar una operación de suma de un número real y otro complejo
+x1=5 + 7j
+print(x1)
+#13.	Realizar una operación de multiplicación
+t1=20 * 5
+print(t1)
+#Mostrar el resultado de elevar 2 a la octava potencia
+print(2**8)
+#15.	Obtener el cociente de la división de 27 entre 4 en una variable y luego mostrarla
+b1=27/4
+print(b1)
+#De la división anterior solamente mostrar la parte entera
+b2=27//4
+print(b2)
+#17.De la división de 27 entre 4 mostrar solamente el resto
+b3=27%4
+print(b3)
+#18.Utilizando como operandos el número 4 y los resultados obtenidos en los puntos 16 y 17. Obtener 27 como resultado
+b4=(4*6)+3
+print(b4)
+#19.	Utilizar el operador "+" en una operación donde intervengan solo variables alfanuméricas
+b6="¡Hola Mundo!" + ","+ "¡Ya estoy aquí!"
+print(b6)
+#20.Evaluar si "2" es igual a 2. ¿Por qué ocurre eso?
+2 == '2'
+print("esto ocurre porque no se pueden comparar dos tipos de datos diferentes")
